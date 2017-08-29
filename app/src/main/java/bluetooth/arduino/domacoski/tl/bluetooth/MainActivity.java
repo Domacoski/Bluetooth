bc1392edf0674cedb78ef479f7bb2d44424ef392
Thiago <tl.domacoski@gmail.com>
2017-08-29 11:49:36 -0300

    Selecao de device

--- app/src/main/java/bluetooth/arduino/domacoski/tl/bluetooth/MainActivity.java
+++ app/src/main/java/bluetooth/arduino/domacoski/tl/bluetooth/MainActivity.java
@@ -1,18 +1,36 @@
 package bluetooth.arduino.domacoski.tl.bluetooth;
 
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    BluetoothAdapter mBluetoothAdapter;
+    private Integer ENABLE_BLUETOOTH = 212;
+
+    private BluetoothAdapter mBluetoothAdapter;
     private BluetoohReceiver mBluetoohReceiver;
+    private BluetoothDevice mDevice = null;
+    private DeviceSelect mDeviceSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +53,70 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         if(!mBluetoothAdapter.isEnabled()){
             enableBluetooh();
+        }else{
+            if(null == mDevice){
+                openDeviceSelect();
+            }
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == ENABLE_BLUETOOTH){
+            if(resultCode == RESULT_OK){
+                openDeviceSelect();
+            }else{
+                Toast.makeText(this, "O aplicativo só funciona com o Bluetooth Ligado!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    public void openDeviceSelect(){
+        if(null != mDeviceSelect){
+            if(mDeviceSelect.isShowing()){
+                return;
+            }
+            mDeviceSelect = null;
+        }
+
+        mDeviceSelect = new DeviceSelect(this);
+        mDeviceSelect.show();
+    }
+    public void closeDeviceSelect(){
+        if(null != mDeviceSelect){
+            if(mDeviceSelect.isShowing()){
+                mDeviceSelect.dismiss();
+            }
+        }
+    }
+
+    public void setDevice(final BluetoothDevice mDevice){
+        this.mDevice = mDevice;
+        updateUIFromDevice();
+    }
+
+    private void updateUIFromDevice(){
+        if(null == mDevice){
+            setTitle("Nenhum dispositivo");
+        }else{
+            setTitle(mDevice.getName());
+        }
+
+    }
 
     public void enableBluetooh(){
         final Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(turnOn, 0);
+        startActivityForResult(turnOn, ENABLE_BLUETOOTH);
     }
-
-
 
     public class BluetoohReceiver extends BroadcastReceiver {
         @Override
@@ -60,16 +130,76 @@ public class MainActivity extends AppCompatActivity {
                         enableBluetooh();
                         break;
                     case BluetoothAdapter.STATE_TURNING_OFF:
-                        Toast.makeText(context, "Turning Bluetooth off...", Toast.LENGTH_SHORT).show();
+                        setDevice(null);
+                        Toast.makeText(context, "Desligando Bluetooth...", Toast.LENGTH_SHORT).show();
                         break;
                     case BluetoothAdapter.STATE_ON:
-                        Toast.makeText(context, "Bluetooth on!!!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, "Bluetooth online!", Toast.LENGTH_SHORT).show();
                         break;
                     case BluetoothAdapter.STATE_TURNING_ON:
                         Toast.makeText(context, "Aguarde...", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
+        }
+    }
+
+    class DeviceSelect extends Dialog{
+        public DeviceSelect(@NonNull Context context) {
+            super(context);
+        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE); //before
+            setContentView(R.layout.dialog_device);
+            final List<BluetoothDevice> devices = new ArrayList<>();
+            for(final BluetoothDevice d : mBluetoothAdapter.getBondedDevices()){
+                devices.add(d);
+            }
+            final AdapterDevice adapterDevice = new AdapterDevice(getBaseContext(), devices);
+            final ListView deviceList = ListView.class.cast(findViewById(R.id.deviceList));
+            deviceList.setAdapter(adapterDevice);
+        }
+    }
+    class AdapterDevice extends ArrayAdapter<BluetoothDevice>{
+        private final List<BluetoothDevice> devices;
+        private final LayoutInflater inflater;
+        public AdapterDevice(final Context context, final List<BluetoothDevice> devices){
+            super(context, 0);
+            this.devices = devices;
+            this.inflater = LayoutInflater.class.cast(context.getSystemService(LAYOUT_INFLATER_SERVICE));
+        }
+        @Nullable
+        @Override
+        public BluetoothDevice getItem(int position) {
+            return devices.get(position);
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            final View view = inflater.inflate(R.layout.adapter_device, parent, false);
+            final BluetoothDevice device = getItem(position);
+            TextView.class.cast(view.findViewById(R.id.name)).setText(device.getName());
+            String uuid = ". . .";
+            if(device.getUuids() != null){
+                if(null != device.getUuids()[0]){
+                    uuid = device.getUuids()[0].getUuid().toString();
+                }
+            }
+            TextView.class.cast(view.findViewById(R.id.description)).setText(uuid);
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    setDevice(device);
+                    closeDeviceSelect();
+                }
+            });
+            return view;
+        }
+        @Override
+        public int getCount() {
+            return devices.size();
         }
     }
 
