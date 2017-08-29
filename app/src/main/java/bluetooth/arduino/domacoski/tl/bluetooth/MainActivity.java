401288e7764ef58324f4b3986b71ffbdf5f71fed
Thiago <tl.domacoski@gmail.com>
2017-08-29 10:22:06 -0300

    controle do status do bluetooh

--- app/src/main/java/bluetooth/arduino/domacoski/tl/bluetooth/MainActivity.java
+++ app/src/main/java/bluetooth/arduino/domacoski/tl/bluetooth/MainActivity.java
@@ -1,13 +1,77 @@
 package bluetooth.arduino.domacoski.tl.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter mBluetoothAdapter;
+    private BluetoohReceiver mBluetoohReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoohReceiver = new BluetoohReceiver();
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mBluetoohReceiver, filter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(null != mBluetoohReceiver){
+            unregisterReceiver(mBluetoohReceiver);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!mBluetoothAdapter.isEnabled()){
+            enableBluetooh();
+        }
+    }
+
+
+    public void enableBluetooh(){
+        final Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(turnOn, 0);
+    }
+
+
+
+    public class BluetoohReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        enableBluetooh();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Toast.makeText(context, "Turning Bluetooth off...", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Toast.makeText(context, "Bluetooth on!!!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Toast.makeText(context, "Aguarde...", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }
+    }
+
+
 }
